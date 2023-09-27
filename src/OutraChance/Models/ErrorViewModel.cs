c35ccc403c770032bf6b3722a2cd92d451137d28
user\rodrigo <Rodrigo>
2023-09-26 21:50:50 -0300

    Primeiro commit do projeto .NET Core

---
+++ src/OutraChance/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace OutraChance.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
