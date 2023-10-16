fae42e8e9be62ed9d275d247e083141e7786c011
Gabriel Vieira <126190493+gabrielbvieira90@users.noreply.github.com>
2023-10-16 16:01:37 -0300

    Push das migrations e Models, Usuario e Anuncio

---
+++ src/OutraChance/Models/Usuario.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace OutraChance.Models
+{
+    [Table("Usuarios")]
+
+    public class Usuario
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "O preenchimento do Nome é obrigatório!")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "O preenchimento do CPF é obrigatório!")]
+        public int Cpf { get; set; }
+
+        [Required(ErrorMessage = "O preenchimento da Data de Nascimento é obrigatório!")]
+        public DateTime Data_Nascimento { get; set; }
+
+        [Required(ErrorMessage = "O preenchimento do Telefone é obrigatório!")]
+        public int Telefone { get; set; }
+
+        [Required(ErrorMessage = "O preenchimento do E-mail é obrigatório!")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "O preenchimento da Senha é obrigatório!")]
+        public string Senha { get; set; }
+
+        public int Avatar { get; set; }
+    }
+}
