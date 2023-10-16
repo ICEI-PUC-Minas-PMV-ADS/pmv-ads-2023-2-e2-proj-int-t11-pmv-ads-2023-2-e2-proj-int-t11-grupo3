a889216d855e3c9d11ee2c86f26c291cfa6cb5b6
Gabriel Vieira <126190493+gabrielbvieira90@users.noreply.github.com>
2023-10-16 16:01:37 -0300

    Pull das migrations e Models, Usuario e Anuncio

---
+++ src/OutraChance/Models/Anuncio.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OutraChance.Models
+{
+    public class Anuncio
+    {
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "O preenchimento do Titulo é obrigatório!")]
+        public string Titulo { get; set; }
+
+        [Required(ErrorMessage = "O preenchimento da Descrição é obrigatório!")]
+        public string Descricao { get; set; }
+
+        [Required(ErrorMessage = "O preenchimento do Preço é obrigatório!")]
+        public decimal Preco { get; set; }
+
+        [Required(ErrorMessage = "O preenchimento da Cidade é obrigatório!")]
+        public string Cidade { get; set; }
+
+        [Required(ErrorMessage = "O preenchimento do Estado é obrigatório!")]
+        public string Estado { get; set; }
+
+        [Required(ErrorMessage = "O preenchimento da Status é obrigatório!")]
+        public string Status { get; set; }
+
+        public int Id_Usuario { get; set; }
+    }
+}
+
